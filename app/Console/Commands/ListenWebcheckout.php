0d84a53e03036e9887debe48701f9e0f2c10322e
Luis A. Jaimes <luisyi1998@gmail.com>
2022-03-24 08:45:04 -0500

    Feat: Command to check transaction status.

---
+++ app/Console/Commands/ListenWebcheckout.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Actions\QueryInvoiceAction;
+use App\Constants\InvoiceStatus;
+use App\Models\Invoice;
+use App\Services\WebCheckoutService;
+use Illuminate\Console\Command;
+
+class ListenWebcheckout extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'webcheckout:verify';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(WebCheckoutService $webCheckout)
+    {
+        $invoices = Invoice::whereNotNull('request_id')
+            ->where('invoice_status', InvoiceStatus::PENDING)
+            ->get();
+
+        foreach ($invoices as $invoice) {
+            QueryInvoiceAction::excecute($webCheckout, $invoice);    
+        }
+        
+        return self::SUCCESS;
+    }
+}
