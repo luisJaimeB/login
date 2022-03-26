e967ee2ade97c498c34cd75b86fb1df16e18ade8
Luis A. Jaimes <luisyi1998@gmail.com>
2022-03-26 16:52:31 -0500

    test: Checkout and invoices test

---
+++ tests/Feature/CheckoutTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Image;
+use App\Models\Product;
+use App\Models\User;
+use Database\Seeders\PermissionSeeder;
+use Database\Seeders\RoleSeeder;
+use Gloudemans\Shoppingcart\Facades\Cart;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CheckoutTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    /** @test */
+    public function authorized_user_can_access_to_checkout ()
+    {
+        $this->seed([RoleSeeder::class]);
+
+        /** @var \App\Models\User $user */
+        $user = User::factory()->enabled()->create();
+        $user->assignRole('User');
+
+        /** @var \App\Models\Product $product */
+        $product = Product::factory()->create();
+        Image::factory()->create([
+        'product_id' => $product->id
+        ]);
+
+        Cart::add(
+            $product->id,
+            $product->name,
+            $product->quantity,
+            $product->price
+        );
+
+        $response = $this->actingAs($user)->get(route('payments.index'));
+
+        $response->assertOk()
+            ->assertViewIs('products.checkout')
+            ->assertSeeText($product->name)
+            ->assertRedirect(route('payments.store'));
+    }
+}
---
+++ tests/Feature/InvoiceTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Http\Response;
+use Database\Seeders\RoleSeeder;
+use Database\Seeders\PermissionSeeder;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class InvoiceTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function authorized_user_can_access_to_purchace_history ()
+    {
+        $this->seed([RoleSeeder::class, PermissionSeeder::class]);
+
+        /** @var \App\Models\User $user */
+        $user = User::factory()->enabled()->create();
+        $user->assignRole('Admin');
+
+        $response = $this->actingAs($user)->get(route('invoices.index'));
+
+        $response->assertOk()
+        ->assertViewIs('invoices.index');
+    }
+
+    /** @test */
+    public function unauthorized_user_can_not_access_to_purchace_history ()
+    {
+        $this->seed([RoleSeeder::class, PermissionSeeder::class]);
+        
+        /** @var \App\Models\User $user */
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->get(route('invoices.index'));
+
+        $response->assertStatus(Response::HTTP_FORBIDDEN)
+            ->assertRedirect(route('login'));
+    }
+}
