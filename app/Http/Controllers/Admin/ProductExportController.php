01c9e44a1384c14d3d5677bbd3e103cda7acddeb
SeverusReaper <39748335+luisJaimeB@users.noreply.github.com>
2022-04-20 15:05:49 -0500

    feat: Exports module added.

---
+++ app/Http/Controllers/Admin/ProductExportController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Exports\ProductsExport;
+use App\Http\Controllers\Controller;
+use Maatwebsite\Excel\Facades\Excel;
+
+class ProductExportController extends Controller
+{
+    public function export()
+    {
+        return Excel::download(new ProductsExport, 'products.xlsx');
+    }
+}
