137c1289c7430a03026b8aff53cea13a988c333b
SeverusReaper <39748335+luisJaimeB@users.noreply.github.com>
2022-05-13 12:26:30 -0500

    Feat: Model and table for imports.

---
+++ database/migrations/2022_05_10_203713_create_imports_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Constants\ImportStatus;
+use App\Constants\ImportType;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImportsTable extends Migration
+{
+    
+    public function up(): void
+    {
+        Schema::create('imports', function (Blueprint $table) {
+            $table->id();
+            $table->string('file_name');
+            $table->json('errors')->nullable();
+            $table->enum('import_type', ImportType::toArray());
+            $table->enum('import_status', ImportStatus::toArray())->default(ImportStatus::PENDING);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('imports');
+    }
+}
